5d6117aedc31c62521e825093df53c03fb2517fe
harrynguyen97 <harry.nguyen1597@gmail.com>
2018-08-22 21:33:47 +0930

    Add conditional partion disk. If partioned, then not partion again

--- install_arch.sh
+++ install_arch.sh
@@ -1,6 +1,15 @@
-parted /dev/sda \
-	mklabel msdos \
-	mkpart primary ext4 1MiB 20G \
-	set 1 boot on \
-	mkpart primary linux-swap 20G 22G \
-	mkpart primary ext4 22G 100%
+if [$(/sbin/sfdisk - d /dev/sda) == ""]
+then 
+	parted /dev/sda \
+		mklabel msdos \
+		mkpart primary ext4 1MiB 20G \
+		set 1 boot on \
+		mkpart primary linux-swap 20G 22G \
+		mkpart primary ext4 22G 100%
+fi
+echo "Ola!"
+# mkfs.ext4 /dev/sda1
+# mkfs.ext4 /dev/sda3
+# mkswap /dev/sda2
+# swapon /dev/sda2
+
