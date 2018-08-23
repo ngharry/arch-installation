43e4eb7575a0aaf3e330918828a56639d6e6e58e
harrynguyen97 <ng.harry97@gmail.com>
2018-08-23 08:16:29 +0000

    split to functions

--- install_arch.sh
+++ install_arch.sh
@@ -1,47 +1,67 @@
-echo "Partioning disk..."
-parted /dev/sda \
-	mklabel msdos \
-	mkpart primary ext4 1MiB 20G \
-	set 1 boot on \
-	mkpart primary linux-swap 20G 22G \
-	mkpart primary ext4 22G 100%
-echo "Finished."
+setup() {
+	echo "Partioning..."
+	partion
+	echo "Finished."
 
-echo "Making file system..."	
-mkfs.ext4 /dev/sda1
-mkfs.ext4 /dev/sda3
-mkswap /dev/sda2
-swapon /dev/sda2
-echo "Finished."
+	echo "Formating..."
+	format_fs
+	echo "Finished."
 
-echo "Mounting file system..."
-mount /dev/sda1 /mnt
-mkdir /mnt/home
-mount /dev/sda3 /mnt/home
-echo "Finished."
+	echo "Mounting..."
+	mount_disk
+	echo "Finished"
+	
+	pacstrap /mnt base base-devel
+	genfstab -U /mnt >> /mnt/etc/fstab
+	arch-chroot /mnt /bin/bash
+}
 
-pacstrap /mnt base base-devel
+partion() {
+	parted /dev/sda \
+    		mklabel msdos \
+        	mkpart primary ext4 1MiB 20G \
+        	set 1 boot on \
+        	mkpart primary linux-swap 20G 22G \
+		mkpart primary ext4 22G 100%
+}
 
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt /bin/bash
-rm /etc/localtime
-ln -s /usr/share/zoneinfo/Australia/Adelaide /etc/localtime
-hwclock --systohc --utc
+format_fs() {
+	mkfs.ext4 /dev/sda1
+	mkfs.ext4 /dev/sda3
+	mkswap /dev/sda2
+	swapon /dev/sda2
+}
 
-sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
-locale-gen
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-export LANG=en_US.UTF-8
+mount_disk() {
+	mount /dev/sda1 /mnt
+	mkdir /mnt/home
+	mount /dev/sda3 /mnt/home
+}
+if [ "$1" == "setup" ]
+then
+	setup
+else 
+	echo "Configure has not written yet."
+fi
 
-echo harry-arch > /etc/hostname
-systemctl enable dhcpcd.service
+#rm /etc/localtime
+#ln -s /usr/share/zoneinfo/Australia/Adelaide /etc/localtime
+#hwclock --systohc --utc
 
-passwd && pacman -S grub os-prober 
+#sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
+#locale-gen
+#echo LANG=en_US.UTF-8 > /etc/locale.conf
+#export LANG=en_US.UTF-8
 
-grub-install /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg
+#echo harry-arch > /etc/hostname
+#systemctl enable dhcpcd.service
 
-pacman -Syu
+#passwd && pacman -S grub os-prober 
 
-exit
+#grub-install /dev/sda
+#grub-mkconfig -o /boot/grub/grub.cfg
+
+#pacman -Syu
+
+#exit
 
