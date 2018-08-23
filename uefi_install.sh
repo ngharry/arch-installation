b614964ec39fda359d7ddb9ea3e0459b9ca86253
harrynguyen97 <harry.nguyen1597@gmail.com>
2018-08-23 23:41:33 +0930

    Test partion

--- uefi_install.sh
+++ uefi_install.sh
@@ -1,167 +1,87 @@
+# CONF_NAME=configure.sh
+# configure_sh_link=https://raw.githubusercontent.com/harrynguyen97/arch-installation/master/configure.sh
+
+# $1: FAT32
+# $2: swap
+# $3: /
 partion() {
+	local fat_size=$1
+	local swap_size=$2
+
+	local i=1
+
 	parted /dev/sda \
 		mklabel gpt \
-		mkpart ESP fat32 1M 513M \
+		mkpart ESP fat32 1MiB $(($fat_size + 1))MiB \
 		set 1 boot on \
-		mkpart primary linux-swap 513M 2561M \
-		mkpart primary ext4 2561M 100%
+		mkpart primary linux-swap $(($fat_size + 2) $(($swap_size + $fat_size + 2))MiB \
+		mkpart primary ext4 $(($swap_size + $fat_size + 3))MiB 100%
 }
 
-format_partion() {
-	mkfs.fat -F32 /dev/sda1
-	mkfs.ext4 /dev/sda3
-	mkswap /dev/sda2
-	swapon /dev/sda2
-}
+# format_partion() {
+# 	mkfs.fat -F32 /dev/sda1
+# 	mkfs.ext4 /dev/sda3
+# 	mkswap /dev/sda2
+# 	swapon /dev/sda2
+# }
 
-mount_fs() {
-	mount /dev/sda3 /mnt
-	mkdir /mnt/boot
-	mount /dev/sda1 /mnt/boot
-}
+# mount_fs() {
+# 	mount /dev/sda3 /mnt
+# 	mkdir /mnt/boot
+# 	mount /dev/sda1 /mnt/boot
+# }
 
-install_base() {
-	pacstrap /mnt base base-devel
-}
+# install_base() {
+# 	echo 'Server = http://mirrors.kernel.org/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
+# 	pacstrap /mnt base base-devel
+# }
 
-generate_fstab() {
-	genfstab -U /mnt >> /mnt/etc/fstab
-}
+# generate_fstab() {
+# 	genfstab -U /mnt >> /mnt/etc/fstab
+# }
 
-change_root() {
-	chmod +x /mnt/$1
-	arch-chroot /mnt ./$1
-}
+# change_root() {
+# 	chmod +x /mnt/$1
+# 	arch-chroot /mnt ./$1
+# }
 
-unmount_disk() {
-	umount /mnt/boot
-	umount /mnt
-}
+# unmount_disk() {
+# 	umount /mnt/boot
+# 	umount /mnt
+# }
 
-setup() {
-	echo "Disk partioning..."
-	partion
-	echo "Finished."
+# setup() {
+# 	echo "Disk partioning..."
+# 	partion
+# 	echo "Finished."
 
-	echo "Formating partions..."
-	format_partion
-	echo "Finished."
+# 	echo "Formating partions..."
+# 	format_partion
+# 	echo "Finished."
 
-	echo "Mounting file system..."
-	mount_fs
-	echo "Finished."
+# 	echo "Mounting file system..."
+# 	mount_fs
+# 	echo "Finished."
 
-	echo "Installing base and base devel..."
-	install_base
-	echo "Finished."
+# 	echo "Installing base and base devel..."
+# 	install_base
+# 	echo "Finished."
 
-	echo "Generating file system table..."
-	generate_fstab
-	echo "Finished."
+# 	echo "Generating file system table..."
+# 	generate_fstab
+# 	echo "Finished."
 
-	cat > /mnt/configure.sh <<"EOF"
-TIMEZONE=Australia/Adelaide
-LANGUAGE=en_US.UTF-8
+# 	curl $configure_sh_link > /mnt/$CONF_NAME
+# 	change_root $CONF_NAME
 
-set_timezone() {
-	if [ -f /etc/localtime ]
-	then 
-		rm /etc/localtime
-		echo "Removed existed /etc/localtime."
-	fi
+# 	if [ -f /mnt/$CONF_NAME ]; then
+# 		echo 'ERROR: Failed during chroot. Try again.'
+# 	else
+#     	echo 'Unmounting filesystems'
+#     	unmount_disk
+#     	echo 'Finished. You should reboot system for applying changes.'
+# 	fi
+# }
 
-	ln -sf /usr/share/zoneinfo/$1 /etc/localtime
-	hwclock --systohc 
-}
-
-set_language() {
-	sed -i 's/#$1/$1/g' /etc/locale.gen
-	locale-gen
-	echo LANG=$1 > /etc/locale.conf
-	export LANG=$1
-}
-
-set_hostname() {
-	read -p 'Enter host name: ' HOSTNAME
-	echo $HOSTNAME > /etc/hostname
-}
-
-configure_network() {
-	pacman -S networkmanager && systemctl enable NetworkManager
-}
-
-set_root_password() {
-	passwd
-	if [ $? -ne 0 ]; then
-		exit
-	fi
-}
-
-install_bootloader() {
-	pacman -S grub efibootmgr
-	grub-install --target=x86_64-efi --efi-directory=/boot
-	if [ $? -ne 0 ]; then
-		exit
-	fi
-
-	grub-mkconfig -o /boot/grub/grub.cfg
-	if [ $? -ne 0 ]; then
-		exit
-	fi
-}
-
-patch_for_virtualbox() {
-	mkdir /boot/EFI/boot
-	cp /boot/EFI/arch/grubx64.efi /boot/EFI/boot/bootx64.efi
-}
-
-configure() {
-	echo "Setting timezone..."
-	set_timezone $TIMEZONE
-	echo "Finished."
-
-	echo "Setting language..."
-	set_language $LANGUAGE
-	echo "Finished."
-
-	echo "Setting host name..."
-	set_hostname
-	echo "Changed hostname successfully."
-
-	echo "Configuring network..."
-	configure_network
-	echo "Finished."
-
-	echo "Setting root password..."
-	set_root_password
-
-	echo "Preparing to install bootloader..."
-	install_bootloader
-
-	# fix bug for virtualbox only
-	echo "Fixing bug for virtualbox..."
-	patch_for_virtualbox
-	echo "Finished." 
-
-	pacman -Syu
-	echo "Full system upgraded."
-}
-
-configure
-rm configure.sh
-exit
-EOF
-	change_root configure.sh
-
-	if [ -f /mnt/configure.sh ]
-    then
-        echo 'ERROR: Something failed inside the chroot. Try again.'
-    else
-        echo 'Unmounting filesystems'
-        unmount_disk
-        echo 'Finished. You should reboot system for applying changes.'
-    fi
-}
-
-setup
+# setup
 
